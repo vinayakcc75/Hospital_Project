80676034e724879837d28095bbd30eb2ec5056f5
vinayakcc75 <46587781+vinayakcc75@users.noreply.github.com>
2020-06-04 09:07:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import './App.css';
+import LandingPage from './Components/Landing Page/LandingPage';
+
+class App extends Component{
+render(){
+  return (
+	    <div className="App">	 
+            <LandingPage />
+	    </div>
+  	);
+	}
+}
+
+export default App;
+//npm install node-sass
